36923d0a3255c7adce6f5ae37555b7ea004074de
Ankit <74355532+Drago215193@users.noreply.github.com>
2024-05-15 19:38:32 +0000

    Added 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const Application_Id = 215193;
+let Application_Email = "drago@google.com";
+let Application_Password = "12345";
+let Applcation_state;
+
+console.log(Application_Id);
+
+console.table([Application_Id, Application_Email, Application_Password, Applcation_state]);
