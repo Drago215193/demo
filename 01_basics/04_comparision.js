0ce80ce680546c73fd1cca10c7d445462b85c6f4
Ankit <74355532+Drago215193@users.noreply.github.com>
2024-05-16 19:04:28 +0000

    added 04_comparision.js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0); //null value is changed to zero
+console.log(null >= 0); //null is changed to zero in line 12, so zero equal zero
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
